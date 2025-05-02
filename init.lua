539f54aa67971f6df69139f42d5d313b14389ad3
mi-b <michaelbaumann@bluewin.ch>
2025-05-02 14:26:42 +0200

    basic setup; catppucino colorscheme; telescope; treesitter; neo-tree

---
+++ init.lua
@@ -0,0 +1,99 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.g.mapleader = " "
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+{
+'nvim-telescope/telescope.nvim', tag = '0.1.8',
+dependencies = { 'nvim-lua/plenary.nvim' }
+},
+{{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}},
+{
+"nvim-neo-tree/neo-tree.nvim",
+branch = "v3.x",
+dependencies = {
+"nvim-lua/plenary.nvim",
+"nvim-tree/nvim-web-devicons",
+"MunifTanjim/nui.nvim",
+},
+lazy = false, -- neo-tree will lazily load itself
+opts = {
+}
+},
+}
+local opts = {}
+
+require("lazy").setup(plugins, opts)
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+
+
+local config = require("nvim-treesitter.configs")
+config.setup({
+  ensure_installed = {"lua", "javascript"},
+  highlight = { enable = true },
+  indent = { enable = true },
+})
+
+require("catppuccin").setup()
+vim.cmd.colorscheme "catppuccin"
+
+
+-- require("lazy").setup({
+--   spec = {
+--     -- add LazyVim and import its plugins
+--     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+--     -- import/override with your plugins
+--     { import = "plugins" },
+--   },
+--   defaults = {
+--     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
+--     -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
+--     lazy = false,
+--     -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
+--     -- have outdated releases, which may break your Neovim install.
+--     version = false, -- always use the latest git commit
+--     -- version = "*", -- try installing the latest stable version for plugins that support semver
+--   },
+--   install = { colorscheme = { "tokyonight", "habamax" } },
+--   checker = {
+--     enabled = true, -- check for plugin updates periodically
+--     notify = false, -- notify on update
+--   }, -- automatically check for plugin updates
+--   performance = {
+--     rtp = {
+--       -- disable some rtp plugins
+--       disabled_plugins = {
+--         "gzip",
+--         -- "matchit",
+--         -- "matchparen",
+--         -- "netrwPlugin",
+--         "tarPlugin",
+--         "tohtml",
+--         "tutor",
+--         "zipPlugin",
+--       },
+--     },
+--   },
+-- })
+--

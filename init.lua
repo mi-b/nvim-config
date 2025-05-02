7e30a123bc7ea6ff3cef9a0bd7546fb2f8e67f3c
mi-b <michaelbaumann@bluewin.ch>
2025-05-02 18:50:39 +0200

    vim-options and lualine

--- init.lua
+++ init.lua
@@ -1,11 +1,3 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.opt.autoindent = true
-vim.opt.smartindent = false
-vim.g.mapleader = " "
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
@@ -22,47 +14,5 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local opts = {}
-
+require("vim-options")
 require("lazy").setup("plugins")
-
-
-
--- require("lazy").setup({
---   spec = {
---     -- add LazyVim and import its plugins
---     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
---     -- import/override with your plugins
---     { import = "plugins" },
---   },
---   defaults = {
---     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
---     -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
---     lazy = false,
---     -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
---     -- have outdated releases, which may break your Neovim install.
---     version = false, -- always use the latest git commit
---     -- version = "*", -- try installing the latest stable version for plugins that support semver
---   },
---   install = { colorscheme = { "tokyonight", "habamax" } },
---   checker = {
---     enabled = true, -- check for plugin updates periodically
---     notify = false, -- notify on update
---   }, -- automatically check for plugin updates
---   performance = {
---     rtp = {
---       -- disable some rtp plugins
---       disabled_plugins = {
---         "gzip",
---         -- "matchit",
---         -- "matchparen",
---         -- "netrwPlugin",
---         "tarPlugin",
---         "tohtml",
---         "tutor",
---         "zipPlugin",
---       },
---     },
---   },
--- })
---

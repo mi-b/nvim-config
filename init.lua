df45685664e3b69addd218e07d82321498fbeee1
mib <michael.baumann@helbling.ch>
2025-05-06 13:23:14 +0200

    key mappings

--- init.lua
+++ init.lua
@@ -15,4 +15,5 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("vim-options")
+require("mappings")
 require("lazy").setup("plugins")
---
+++ lua/mappings.lua
@@ -0,0 +1,13 @@
+
+local map = vim.keymap.set
+local opts ={ noremap = true} -- add silent = true if needed
+
+-- No clipboard override with n:x, {n, v}:s, c, Del and v:p
+map("n", "x", '"_x', opts)  
+map("n", "X", '"_X', opts)  
+map({"n", "v"}, "s", '"_s', opts)  
+map({"n", "v"}, "S", '"_S', opts)  
+map({"n", "v"}, "c", '"_c', opts)  
+map({"n", "v"}, "C", '"_C', opts)  
+map({"n", "v"}, "<Del>", '"_x', opts)  
+map("v", "p" , "pgvy", opts)

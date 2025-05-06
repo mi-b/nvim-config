9b93e6dc891725e1fe22d180e12fc32902e7d03d
mi-b <michaelbaumann@bluewin.ch>
2025-05-06 22:02:57 +0200

    toggle neotree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,14 +1,14 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
-  },
-  lazy = false,
-  opts = {},
-  config = function()
-    vim.keymap.set("n", "<leader>e", ":Neotree filesystem reveal left<CR>", { desc = "Reveal Neotree" })
-  end,
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	lazy = false,
+	opts = {},
+	config = function()
+		vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>", { desc = "Toggle neo-tree" })
+	end,
 }

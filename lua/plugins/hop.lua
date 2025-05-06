a890f4ba1493c8b2e657a1fc3fa6d6a06117bbdf
mib <michael.baumann@helbling.ch>
2025-05-06 16:30:00 +0200

    options for hop

--- lua/plugins/hop.lua
+++ lua/plugins/hop.lua
@@ -1,13 +1,13 @@
 return {
-'smoka7/hop.nvim',
-version = "*",
-opts = {
-keys = 'etovxqpdygfblzhckisuran'
-},
-lazy = false,
-config = function()
-  require('hop').setup({})
-  vim.keymap.set({ "n", "v"}, '<leader>jk', ":HopWord<CR>", {desc = "Hop Word"})
-  vim.keymap.set({ "n", "v"}, '<leader>jh', ":HopLine<CR>", {desc = "Hop Line"})
-end
+  'smoka7/hop.nvim',
+  version = "*",
+  lazy = false,
+  config = function()
+    opts = {
+      keys = 'etovxqpdygfblzhckisuran'
+    }
+    require('hop').setup(opts)
+    vim.keymap.set({ "n", "v"}, '<leader>jk', ":HopWord<CR>", {desc = "Hop Word"})
+    vim.keymap.set({ "n", "v"}, '<leader>jh', ":HopLine<CR>", {desc = "Hop Line"})
+  end
 }

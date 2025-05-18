296213cda39137114b9f7ef720123ab4c2bb1de6
mi-b <michaelbaumann@bluewin.ch>
2025-05-18 10:11:47 +0200

    format on save

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,26 +1,37 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
+  "nvimtools/none-ls.nvim",
+  config = function()
+    local null_ls = require("null-ls")
 
-		if not require("utils").is_windows() then
-			null_ls.setup({
-				sources = {
-					null_ls.builtins.formatting.stylua,
-					null_ls.builtins.formatting.prettier,
-					-- null_ls.builtins.formatting.rubocop,
-					-- null_ls.builtins.diagnostics.rubocop,
-					-- null_ls.builtins.diagnostics.eslint_d,
-				},
-			})
-		else
-			null_ls.setup({
-				sources = {
-					null_ls.builtins.formatting.stylua,
-				},
-			})
-		end
+    local sources = {}
+    if not require("utils").is_windows() then
+      table.insert(sources, null_ls.builtins.formatting.stylua)
+      table.insert(sources, null_ls.builtins.formatting.prettier)
+      -- table.insert(sources, null_ls.builtins.formatting.rubocop)
+      -- table.insert(sources, null_ls.builtins.diagnostics.rubocop)
+      -- table.insert(sources, null_ls.builtins.diagnostics.eslint_d)
+    else
+      table.insert(sources, null_ls.builtins.formatting.stylua)
+    end
 
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { desc = "format" })
-	end,
+    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+    null_ls.setup({
+      sources = sources,
+      -- you can reuse a shared lspconfig on_attach callback here
+      on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+              vim.lsp.buf.format({ async = false })
+            end,
+          })
+        end
+      end,
+    })
+
+    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { desc = "format" })
+  end,
 }
